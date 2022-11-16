fdd7e4f22c3414c537346c2ecff5c8ac9f2a707a
sanlokaja <sandyloka790@gmail.com>
2022-11-16 20:37:28 +0700

    docs: add user routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const userRouter = require('./user.router');
 const jobRouter = require('./job.router');
 
 router.use('/api/admin', jobRouter);
+router.use('/api', userRouter);
 
 module.exports = router;
