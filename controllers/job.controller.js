b6bf5f4e6f9142cba6d40559402e4fdc0c11bfcf
sanlokaja <sandyloka790@gmail.com>
2022-11-16 15:02:09 +0700

    feat: add feature crud

---
+++ controllers/job.controller.js
@@ -0,0 +1,176 @@
+const { job } = require("../models");
+
+
+/* -------- Get Job -------- */
+
+const handleGetJob = async(req, res) => {
+
+    try {
+
+        const getJobData = await job.findAll();
+
+        res.status(200).send({
+            status: true,
+            message: 'Pekerjaan berhasil didapatkan!',
+            data: getJobData,
+        });
+
+    } catch (err) {
+        res.status(500).send({
+            status: false,
+            message: err.message,
+            data: null,
+        });
+    }
+
+}
+
+/* -------- Get Job -------- */
+
+
+/* -------- Get Job By Id -------- */
+
+const handleGetJobById = async(req, res) => {
+
+    try {
+
+        const { id } = req.params;
+
+        const getJobDataById = await job.findOne({
+            where: { id }
+        });
+
+        if (getJobDataById.id == id) {
+            res.status(200).send({
+                status: true,
+                message: 'Pekerjaan berhasil didapatkan!',
+                data: getJobDataById,
+            });
+        }
+
+    } catch (err) {
+        res.status(500).send({
+            status: false,
+            message: err.message,
+            data: null,
+        });
+    }
+
+}
+
+/* -------- Get Job By Id -------- */
+
+
+/* -------- Create Job -------- */
+
+const handleCreateJob = async (req, res) => {
+
+    try {
+
+        const { job_name } = req.body;
+
+        const createdJob = await job.create({ job_name });
+
+        res.status(201).send({
+            status: true,
+            message: 'Pekerjaan berhasil ditambahkan!',
+            data: createdJob,
+        });
+
+    } catch (err) {
+        res.status(500).send({
+            status: false,
+            message: err.message,
+            data: null,
+        });
+    }
+
+}
+
+/* -------- End Create Job -------- */
+
+
+/* -------- Update Job -------- */
+
+const handleUpdateJob = async(req, res) => {
+
+    try {
+
+        const { id } = req.params
+
+        const { job_name } = req.body;
+
+        const getJobDataById = await job.findOne({
+            where: { id }
+        });
+
+        if (getJobDataById.id == id) {
+            const updatedJob = await job.update({ 
+                job_name 
+            }, {
+                where: { id }
+            });
+
+            res.status(201).send({
+                status: true,
+                message: 'Pekerjaan berhasil diubah!',
+                data: updatedJob,
+            });
+        }
+
+    } catch (err) {
+        res.status(500).send({
+            status: false,
+            message: err.message,
+            data: null,
+        });
+    }
+
+}
+
+/* -------- End Update Job -------- */
+
+
+/* -------- Delete Job -------- */
+
+const handleDeleteJob = async(req, res) => {
+
+    try {
+
+        const { id } = req.params
+
+        const getJobDataById = await job.findOne({
+            where: { id }
+        });
+
+        if (getJobDataById.id == id) {
+            const deletedJob = await job.destroy({
+                where: { id }
+            });
+
+            res.status(201).send({
+                status: true,
+                message: 'Pekerjaan berhasil dihapus!',
+                data: deletedJob,
+            });
+        }
+
+    } catch (err) {
+        res.status(500).send({
+            status: false,
+            message: err.message,
+            data: null,
+        });
+    }
+
+}
+
+/* -------- End Delete Job -------- */
+
+module.exports = { 
+    handleGetJob,
+    handleGetJobById,
+    handleCreateJob,
+    handleUpdateJob,
+    handleDeleteJob
+};
