b90f0b16816c80da37d501d43cfa29a60e7e9444
arraeys <arsensimanihuruk@gmail.com>
2022-11-18 20:14:19 +0700

    Revert "minor changes"
    This reverts commit 34bc893087ee225da0a84a223b9f7530a617bff8.

---
+++ migrations/20221116175858-create-company-profile.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Company_profiles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nama: {
+        type: Sequelize.STRING
+      },
+      alamat: {
+        type: Sequelize.STRING
+      },
+      kontak: {
+        type: Sequelize.STRING
+      },
+      id_user: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Company_profiles');
+  }
+};
