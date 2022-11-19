1d681b2aec29e75afbc40c25e476f0d74349ad6e
V123in4 <vinconloren@gmail.com>
2022-11-19 21:23:24 +0700

    added error handler

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,14 +1,28 @@
 const { users, Profile, sequelize } = require("../models");
-const date = "GETDATE()";
 
 const handleGetAdminById = async (req, res) => {
 	const { id } = req.params;
 
-	const data = await sequelize.query(
-		`select u.id, p.nama, u.email, p.kontak, p.alamat from users u left join profiles p on u.id = p.id_user where u.id = ${id}`
-	);
+	try {
+		const data = await sequelize.query(
+			`select u.id, p.nama, u.email, p.kontak, p.alamat from users u left join profiles p on u.id = p.id_user where u.id = ${id}`
+		);
 
-	res.send(data[0]);
+		const payload = {
+			status: 200,
+			message: "GET sucessful",
+			data: data[0],
+		};
+
+		res.status(200).send(payload);
+	} catch (err) {
+		const payload = {
+			status: 500,
+			message: "something went wrong",
+			data: err,
+		};
+		res.status(500).send(payload);
+	}
 };
 
 module.exports = {
